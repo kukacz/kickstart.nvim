0ce7f1acfcb1dba7335ae467c52126b323ec2aa2
Lukáš Kubín <lukas.kubin@eset.com>
2024-11-25 18:12:11 +0100

    Add Markview plugin

---
+++ lua/custom/plugins/markview.lua
@@ -0,0 +1,11 @@
+return {
+  'OXY2DEV/markview.nvim',
+  enabled = true,
+  lazy = false,
+  ft = { 'markdown', 'norg', 'rmd', 'org', 'vimwiki', 'Avante' },
+  opts = {
+    filetypes = { 'markdown', 'norg', 'rmd', 'org', 'vimwiki', 'Avante' },
+    buf_ignore = {},
+    max_length = 99999,
+  },
+}
